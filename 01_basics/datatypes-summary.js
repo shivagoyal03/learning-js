6378f24918db06f50748e17c5de912680edc6b35
shivagoyal03 <165026204+shivagoyal03@users.noreply.github.com>
2025-11-21 12:35:41 +0000

    summary of datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive - 7types
+// string, number, boolean, null, undefined, symbol, bigint
+
+// refernce = 3 types
+// array, functions, objetcs
+
+const score = 85
+const value = true
+const decimal = 5.95
+const outsidetemp = null
+let userEmail;
+
+
+const id = symbol("123")
+const anotherId = symbol("123")
+console.log(id===anotherId);
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+       */
