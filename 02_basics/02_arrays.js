1ff940dadbcf6900cbcbc7d266c095b6b3e5ed72
shivagoyal03 <165026204+shivagoyal03@users.noreply.github.com>
2025-11-22 14:24:48 +0000

    more methods of arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,20 @@
+// const marvel = ["Thor","ironman","hulk"]
+// const dc = ["batman","superman","flash"]
+
+// // marvel.push(dc)
+// // console.log(marvel);
+// const concatArr = marvel.concat(dc)
+// console.log(concatArr);
+
+// const allNewHeros = [...marvel,...dc]
+// console.log(allNewHeros);
+
+// const Array = [1,2,[3,4,5,6],4,8,[4,[5,6]]]
+// const real_array = Array.flat(Infinity)
+// console.log(real_array);
+
+
+console.log(Array.isArray("shiva"))
+console.log(Array.from("shiva"))
+console.log(Array.from({name:"shiva"}))  // interesting
+
