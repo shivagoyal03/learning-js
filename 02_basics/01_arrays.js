df019a7b79b65528b9db3870eba9f10d75fadb3d
shivagoyal03 <165026204+shivagoyal03@users.noreply.github.com>
2025-11-22 14:02:55 +0000

    methods of arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+const myArr = [0,1,2,3,4,5,6]
+// console.log(myArr[0]);
+// const arr = new Array(1,2,3,4,5)
+
+// methods ----
+
+// arr.push(6) // it adds from end
+// console.log(arr);
+// arr.pop() // it removes from end
+// console.log(arr);
+// arr.unshift(9) // it adds from start
+// console.log(arr);
+// arr.shift() // it removes from start
+// console.log(arr);
+
+// const newArr = arr.join()
+// console.log(arr);
+// console.log(newArr);
+// console.log(newArr[8]);
+
+// slice & splice
+console.log("A " ,myArr);
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B " , myArr);
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
+console.log("C ",myArr);
+
+
+
+
